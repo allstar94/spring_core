a7bc431e4cb20f26d397dd8e8a07717b0ece9b79
allstar94 <alsltar94@gmail.com>
2022-01-13 23:50:25 +0900

    2022-01-13 gg

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -22,4 +28,5 @@ public class MemberServiceImpl implements MemberService{
     public MemberRepository getMemberRepository(){
         return memberRepository;
     }
+
 }
